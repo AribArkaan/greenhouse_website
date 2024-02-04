60e0fe6c5098b953c9214f78701a411a6cd6c994
AribArkaan <130653952+AribArkaan@users.noreply.github.com>
2024-02-05 01:17:28 +0700

    nambah banyak
    informasi 1, dll

--- scripts.js
+++ scripts.js
@@ -134,8 +134,28 @@ document.addEventListener('DOMContentLoaded', function () {
   });
   
 
+  function updateClock() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
 
+    // Menambah nol di depan angka jika angka < 10
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    var timeString = hours + ":" + minutes + ":" + seconds;
+
+    document.getElementById("clock").innerText = timeString;
+
+    setTimeout(updateClock, 1000); // Memanggil fungsi setiap detik
+  }
+
+  // Panggil fungsi pertama kali
+  updateClock();
 
   
+
 
 
